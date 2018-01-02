0e3c779c0ab0cf80ac6583994f44978e5989b088
Mr.Robb <roberto.ariosa@est.fib.upc.edu>
2018-01-02 03:27:18 +0100

    Prompts

--- Genetic Algorithms/main.cpp
+++ Genetic Algorithms/main.cpp
@@ -12,17 +12,28 @@
 
 int main(int argc, const char * argv[]) {
 	string target = "i love you to the moon and back";
+	cout << "Enter the target phrase:" << endl;
+	getline(cin, target);
+
 	int N_ind = 1000;
+	cout << "Enter the number of individuals (recommended from 200 to 1000):" << endl;
+	cin >> N_ind;
+
 	float mutationRate = 0.01;
-	
-	
-	vector<Population> populations = {};
+	cout << "Enter the mutation rate (recommended from 0.001 to 0.05):" << endl;
+	cin >> mutationRate;
+
+	printf("Target phrase: %s\n", target.c_str());
+	printf("Number of individuals: %d\n", N_ind);
+	printf("Mutation rate: %f\n", mutationRate);
+
+	vector<Population> populations;
 	Population p(target, mutationRate, N_ind, 0);
 	populations.push_back(p);
 	bool finished = false;
 	do {
 		populations.back().calculateFitness();
-		printf("%s\n", populations.back().getBest().getPhrase().c_str());
+		printf("Best specimen: %s\n", populations.back().getBest().getPhrase().c_str());
 		printf("Generation: %d, Average: %f\n", populations.back().getGenerations(), populations.back().getAverage());
 		populations.back().naturalSelection();
 		finished = populations.back().isFinished();
